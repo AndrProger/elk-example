e0b4aa0cb33ffc5d56edae10707f6b6326f601f2
Андрей Андреевич Стариненко <114221320+AndrProger@users.noreply.github.com>
2024-05-01 20:25:55 +0500

    final

--- src/main/kotlin/LogKafkaExample.kt
+++ src/main/kotlin/LogKafkaExample.kt
@@ -1,6 +1,8 @@
 import org.slf4j.LoggerFactory
 
 fun main() {
+
+    println("Hello, World! It's example of using KafkaAppender in Logback.")
     val logger = LoggerFactory.getLogger("main")
 
     logger.info("Hello, World!")
